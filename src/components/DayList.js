6bf41a46e1693215114a4e8b71fcd066961e99f2
Spencer <spencerb242@proton.me>
2022-10-04 20:07:45 -0600

    created DayListItem and DayList components along with storybook testing

---
+++ src/components/DayList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  
+  const dayListItemArr = props.days.map((dayListItem) => {
+    return(
+    <DayListItem
+      key={dayListItem.id}
+      name={dayListItem.name}
+      spots={dayListItem.spots}
+      selected={dayListItem.name === props.day}
+      setDay={props.setDay}
+    />
+    );
+  })
+
+  return (
+    <ul>
+      {dayListItemArr}
+    </ul>
+  );
+
+
+}
